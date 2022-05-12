0e0753e335a6057ee46ad35f1d6cde5af6c778d5
rajkarnikar11 <rajkarnikarsalil11@gmail.com>
2022-05-12 10:47:17 +0545

    single output implemented

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+const menu = document.querySelector(".navbar__burger-container");
+let toggler = 0;
+function toggleMenu() {
+	if (toggler === 0) {
+		toggler = 1;
+		document.querySelector(".navbar__list-container-small").style.opacity =
+			"1";
+		document.querySelector(".navbar__right-small").style.display = "flex";
+	} else {
+		toggler = 0;
+		document.querySelector(".navbar__list-container-small").style.opacity =
+			"0";
+		document.querySelector(".navbar__right-small").style.display = "none";
+	}
+}
+menu.addEventListener("click", toggleMenu);
